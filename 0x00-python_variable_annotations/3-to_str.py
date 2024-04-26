5754bec6581eab1be0dbfc2cf22c55467b67e7e9
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-04-26 21:58:46 +0300

    first commit for question 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' function that returns string representation of a float
+'''
+
+
+def to_str(n: float) -> str:
+    ''' takes float n as argument and returns string representation of a float
+    '''
+    return str(n)
