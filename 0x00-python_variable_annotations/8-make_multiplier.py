a0c5ee2eb25d8227760173837e219f9d7ba20cbb
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-04-26 23:06:20 +0300

    first commit for question 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' function that multiplies a float by multiplier
+'''
+from typing import Callable
+
+
+def make_multiplier(mutiplier: float) -> Callable[[float], float]:
+    ''' returns function that multiplies a float by argument multiplier
+    '''
+    return lambda x: x * mutiplier
