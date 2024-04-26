ed85fd3929301a57f9393b4a7fcc7489bfee363c
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-04-26 21:34:45 +0300

    first commit for question 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' function that concaatinates strings
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' takes string str1 and str2 as arguments and returns them concatinated
+    '''
+    return str1 + str2
